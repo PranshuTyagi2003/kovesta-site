7717eb85668d55a2afbc7620409302421d404bf2
PranshuTyagi2003 <88305222+PranshuTyagi2003@users.noreply.github.com>
2023-06-12 21:03:01 +0530

    Add files via upload

---
+++ build/precache-manifest.38a111e3b5ec80ee0fc76c4aa1ebae8f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7772c2aed20cd6cd4110",
+    "url": "/static/css/main.c8c16bda.chunk.css"
+  },
+  {
+    "revision": "7772c2aed20cd6cd4110",
+    "url": "/static/js/main.7772c2ae.chunk.js"
+  },
+  {
+    "revision": "3c11fcd39aff6eecc4c3",
+    "url": "/static/css/1.13a84fd2.chunk.css"
+  },
+  {
+    "revision": "3c11fcd39aff6eecc4c3",
+    "url": "/static/js/1.3c11fcd3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "19558d1d4418f7f179c2b5b9f020bbc5",
+    "url": "/static/media/code.19558d1d.png"
+  },
+  {
+    "revision": "40fd9bc3ba8c0c1dbccc56287772ce0f",
+    "url": "/static/media/design.40fd9bc3.png"
+  },
+  {
+    "revision": "9687a703fdda4a9ef0c2bd226b433777",
+    "url": "/static/media/app.9687a703.png"
+  },
+  {
+    "revision": "2389d89630b8d626ba17ac1a78b060a7",
+    "url": "/static/media/bg1.2389d896.avif"
+  },
+  {
+    "revision": "c3b5d4050d498a39bbbe0369a327af2a",
+    "url": "/static/media/bg2.c3b5d405.avif"
+  },
+  {
+    "revision": "51b57dbb685291a8adbdc98915f2a495",
+    "url": "/static/media/bg3.51b57dbb.avif"
+  },
+  {
+    "revision": "44e226caf2476f770971dc74fafa7ec8",
+    "url": "/index.html"
+  }
+];
